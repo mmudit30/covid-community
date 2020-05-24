c8c2869a67885a6e388a83cff7d4aa417db5fcdd
Mudit Mittal <mmudit30@gmail.com>
2020-05-24 21:59:09 +0530

    added built

--- app-ang/server.js
+++ app-ang/server.js
@@ -19,6 +19,7 @@ const serverSideClient = new StreamChat(
 );
 
 
+
 app.post('/add-channel', async (req, res) => {
     const { newchannelName, newchannelId, username } = req.body;
     console.log(req.body);    
@@ -87,6 +88,13 @@ app.post('/join', async (req, res) => {
     .status(200)
     .json({ user: { username }, token, api_key: process.env.STREAM_API_KEY });
 });
+
+app.use(express.static(__dirname + '/build'));
+
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
 
 const server = app.listen(process.env.PORT || 3000, () => {
   const { port } = server.address();
