f91e6b1977788f38f568d971f59167de216e49a9
dquach217 <dq6180@rit.edu>
2020-05-23 23:08:32 -0400

    change a bit

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ let router = require('express').Router();
 router.get('/', function(req, res) {
     res.json({
         status: 'API Its Working',
-        message: 'Welcome to Turtle Treasury'
+        message: 'Welcome to Covid-Community'
     });
 });
 // Import user controller
