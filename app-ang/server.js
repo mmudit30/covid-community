588491a24a126d8f94d4589ff740de11a0866658
Mudit Mittal <mmudit30@gmail.com>
2020-05-24 11:32:04 +0530

    first commit

---
+++ app-ang/server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { StreamChat } = require('stream-chat');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize Stream Chat SDK
+
+const serverSideClient = new StreamChat(
+  process.env.STREAM_API_KEY,
+  process.env.STREAM_APP_SECRET
+);
+
+// key 
+// 356h2ygsn8at
+
+// secret
+// 26w37ajn78ya8gu7m7fs4s54bhjk7qc535nrezhyuu6tm9w4weknk3dngpr8wqbs
+
+
+app.post('/join', async (req, res) => {
+  const { username } = req.body;
+  const token = serverSideClient.createToken(username);
+  try {
+    await serverSideClient.updateUser(
+      {
+        id: username,
+        name: username,
+      },
+      token
+    );
+  } catch (err) {
+    // console.log(err);
+  }
+
+  const admin = { id: 'admin' };
+  const channel = serverSideClient.channel('team', 'demo', {
+    name: 'DEmo',
+    image: 'icon-spark.png',
+    created_by: admin,
+  });
+
+  try {
+    await channel.create();
+    await channel.addMembers([username, 'admin']);
+  } catch (err) {
+    // console.log(err);
+  }
+
+  return res
+    .status(200)
+    .json({ user: { username }, token, api_key: process.env.STREAM_API_KEY });
+});
+
+const server = app.listen(process.env.PORT || 5500, () => {
+  const { port } = server.address();
+  console.log(`Server running on PORT ${port}`);
+});
